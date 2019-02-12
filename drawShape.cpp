dc0edc0405d3477ba77fe2020a31110c6ecde520
Yasmine Brooks <brooks_yo@gsc_218_11.mercer.edu>
2019-02-12 13:45:36 -0500

    moving in correct direction

--- drawShape.cpp
+++ drawShape.cpp
@@ -10,9 +10,9 @@ void drawSphere()
 {
        glPushMatrix();
        glTranslated(0.0, -0.3, 0.0);
-       //glTranslated(0.0,0.0,2.0);
-       //glRotated((double)180*theta/M_PI, 1, 0, 0);
-       //glTranslated(0.0,0.0,-2.0);
+       glTranslated(0.0,0.0,2.0);
+       glRotated((double)180*theta/M_PI, 0, 1, 0);
+       glTranslated(0.0,0.0,-2.0);
        glColor3f (0.0,0.0,1.0);
        gluSphere(gluNewQuadric(),
             (GLdouble) 0.3,
@@ -62,7 +62,7 @@ void drawStand()
 	glPushMatrix();
 	glTranslated(0.0, -0.3, 0.0);
         glTranslated(0.0, 0.0, 2.0); //swings with the ball
-        glRotated((double)180*theta/M_PI,1, 0, 0);
+        glRotated((double)180*theta/M_PI,0, 1, 0);
         glTranslated(0.0, 0.0, -2.0);
         glColor3f(1.0, 0.0, 1.0);
         gluCylinder(gluNewQuadric(),
