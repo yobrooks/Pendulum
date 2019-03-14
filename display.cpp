8a43a2e1a7e11c2c49a594512a512f04a67e1cfd
brooks_yo <yasmine.olivia.brooks@live.mercer.edu>
2019-03-13 21:17:06 -0400

    table structure drawn

--- display.cpp
+++ display.cpp
@@ -4,19 +4,22 @@
 #include <math.h>
 #include <iostream>
 
-//static float CAM_A = 15.0, CAM_B = 45.0, CAM_C = 45.0;
-//static float CENTER_X = 0.0, CENTER_Y = 0.0, CENTER_Z = 0.0;
+
 
 void display()
 {
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
     glLoadIdentity();
    // gluLookAt( 20.0,   -10.0, 10.0,  // Eye
-      gluLookAt(CAM_A*sin(CAM_B*M_PI/180.0) * cos(CAM_C*M_PI/180.0),
+     /* gluLookAt(CAM_A*sin(CAM_B*M_PI/180.0) * cos(CAM_C*M_PI/180.0),
  		CAM_A*sin(CAM_B*M_PI/180.0)*sin(CAM_C*M_PI/180.0),
 	        CAM_A*cos(CAM_B*M_PI/180.0),
                 CENTER_X,   CENTER_Y, CENTER_Z,  // Center
-                0.0,   0.0, 1.0); // Up
+                0.0,   0.0, 1.0); // Up*/
+
+      gluLookAt(5.0, 0.0, 0.0,
+		0.0, 0.0, 0.0,
+		0.0, 0.0, 1.0);
 
     glEnable(GL_DEPTH_TEST);
 
@@ -30,6 +33,7 @@ void display()
     //drawScale();
     //drawPeriod();
     defineWalls();
+    drawTable();
     //determines if synch needs to be applied
     if(desiredFR>0.0)
     {
--- drawRoom.cpp
+++ drawRoom.cpp
@@ -1,34 +1,46 @@
 #include "openGl.h"
 #include "prototypes.h"
+#include "globals.h"
+#include <iostream>
 
 void drawLeg()
 {
 	glColor3f(0.3, 0.2, 0.0);
-	glScalef(0.5, 0.5, 3.0);
-	glutWireCube(1.0);
+	glScalef(0.2, 0.2,0.75);
+	glutSolidCube(1.0);
 }
 
 void drawTable()
 {
 	//draw base of table
 	glPushMatrix();
-	glScalef(5.0, 10.0, 1.0);
-	glTranslatef(-0.5, 0.0, -4.5);
+	glScalef(1.3, 2.0, .2);
+	glTranslatef(-2.1, -0.4, -4.0);
 	glColor3f(0.3, 0.2, 0.0);
-	glutWireCube(1.0);
+	glutSolidCube(1.0);
 	glPopMatrix();
 
+//draw four legs
 	glPushMatrix();
-	glTranslatef(-0.6, 4.0, -5.5);
+	glTranslatef(-2.8, -0.1, -1.2);
 	drawLeg();
 	glPopMatrix();
 
 	glPushMatrix();
-	glTranslatef(-0.6, -4.0, -5.5);
+	glTranslatef(-2.8, -1.4, -1.2);
 	drawLeg();
+	glPopMatrix();
+
 	glPushMatrix();
-	
-	
+	glTranslatef(-1.8, -0.1, -1.2);
+	drawLeg();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-1.8, -1.4, -1.2);
+	drawLeg();
+	glPopMatrix();
+		
 }
 
 void defineWalls()
@@ -86,6 +98,19 @@ void defineWalls()
                 glVertex3d(5.0, -5.0, 2.0);
                 glVertex3d(5.0, -5.0, -2.0);
         glEnd();
+}
 
+void drawExtras()
+{
+	//draw door 
+	/*glColor3f(1.0, 1.0, 1.0);
+	glScalef(
+	glTranslatef(
+	glutWireCube(1.0);
 
+	//draw Window
+	glColor3f(0.0, 0.0, 0.0);
+	glScalef(
+	glTranslatef(
+	glutwireCube(1.0);*/
 }
