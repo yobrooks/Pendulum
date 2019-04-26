5173bd721fd74d545845f6aaabec82d84b07b50f
Yasmine Brooks <brooks_yo@gsc_218_10.mercer.edu>
2019-04-26 17:11:54 -0400

    Y axis will not draw--> maybe try taking it out of a 2d context; moving on to lighting

--- graph.cpp
+++ graph.cpp
@@ -4,22 +4,46 @@
 #include "openGl.h"
 #include "prototypes.h"
 #include "globals.h"
-
-/*void textToScreen(const char* message, float x, float y)
+#include <string.h>
+#include <stdio.h>
+void drawAxesLabels()
 {
-        char *charString = (char*) malloc(12*sizeof(char));
-        sprintf(charString, message);
-        glColor3ub(0, 0, 0);
-        makeString(x, y, GLUT_BITMAP_TIMES_ROMAN_24, charString);
-	free(charString);
-}*/
+	char x [] = "X-axis";
+        char y [] = "Y-axis";
+	int strnglngth;
+/*	glPushMatrix();
+	glColor3f(0.0, 1.0, 0.0);
+//	glTranslatef((float)WINDOW_HEIGHT*0.81, (float)WINDOW_HEIGHT*0.25, (float) 1);
+	glTranslatef(100, 100, 0);
+	glRotatef(90.0, 1.0, 0.0, 0.0);
+        glScalef(5.0f, 5.0f, 1.0f);
+        strnglngth = (int) strlen(y);
+        for(int i = 0; i < strnglngth; i++)
+        {
+                glutStrokeCharacter(GLUT_STROKE_ROMAN, y[i]);
+        }
+	glPopMatrix();*/
+	
+	glPushMatrix();
+	glColor3f(0.0, 0.0, 0.0);
+	glTranslatef((float)WINDOW_HEIGHT*0.85, (float)WINDOW_HEIGHT*0.01, (float)1);
+	glScalef(0.1f, 0.1f, 1.0f);
+        strnglngth = (int) strlen(x);
+        for(int i = 0; i < strnglngth; i++)
+        {
+                glutStrokeCharacter(GLUT_STROKE_ROMAN, x[i]);
+        }
+
+        glPopMatrix();
+
+}
 
 void drawAxes(int length)
 {
         glPointSize(1.0);
         glColor3f(0.0, 0.0, 0.0);
 	glPushMatrix();
-	glTranslatef((float)WINDOW_HEIGHT*0.71, (float)WINDOW_HEIGHT*0.39, 0.0);
+	glTranslatef((float)WINDOW_HEIGHT*0.71, (float)WINDOW_HEIGHT*0.39, 1.0);
 	//x axis
         glBegin(GL_LINES);
                 glVertex2i(length, -length-60);
@@ -36,6 +60,9 @@ void drawAxes(int length)
 
 void drawGraphScreen()
 {
+
+
+
 	glDisable(GL_LIGHTING);
 	glMatrixMode(GL_PROJECTION);
         glPushMatrix();
@@ -48,6 +75,8 @@ void drawGraphScreen()
 	
 	drawAxes(225);
 
+	drawAxesLabels();
+
 	glColor3f(1.0, 1.0, 1.0);	
 	glBegin(GL_POLYGON);
 		glVertex2i(WINDOW_HEIGHT*.7, WINDOW_HEIGHT*.4);
@@ -56,7 +85,8 @@ void drawGraphScreen()
 		glVertex2i(WINDOW_HEIGHT, WINDOW_HEIGHT*0.4);
 	glEnd();
 
-        //drawString(50, 100, GLUT_BITMAP_HELVETICA_12, charString);
+	//glRotatef(90, 0.0, 1.0, 1.0);
+      //  drawStrokeText(WINDOW_HEIGHT*0.71, WINDOW_HEIGHT*0.35, 1, y);
 
         glPopMatrix();
         glMatrixMode(GL_PROJECTION);
@@ -67,19 +97,3 @@ void drawGraphScreen()
 }
 
 #endif
-/*
-void drawAxes(int length)
-{
-	glPointSize(1.0);
-	glColor3f(0.0, 0.0, 0.0);
-	glBegin(GL_LINES);
-		glVertex2i(length, 0);
-		glVertex2i(-length, 0);
-	glEnd();
-
-	glBegin(GL_LINES);
-		glVertex2i(0, length);
-		glVertex2i(0, -length);
-	glEnd();
-		
-}*/
